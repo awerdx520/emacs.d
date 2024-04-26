aea5ceb3a251285658659d01118a7c82785cab24
tangxin1 <tangxin1@wps.cn>
2024-04-26 21:21:09 +0800

    feat(project): 设置某些目录不能作为一个 project

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -26,6 +26,8 @@
   ;; Ensure babel's execution environment matches the host buffer's.
   (advice-add #'org-babel-execute-src-block :around #'envrc-propagate-environment))
 
+(defvar +project-ignore-project-list '("~" "/home/thomas" "/")
+  "忽略指定目录作为项目目录.")
 
 (use-package project
   :straight (:type built-in)
@@ -33,19 +35,19 @@
   (setq project-list-file (concat thomas-data-dir "projects"))
   :general
   (thomas-leader
-   "p!" 'project-shell-command ;; 在项目根目录下执行命令
-   "p&" 'project-async-shell-command
-   "pa" 'project-remember-projects-under ;; 添加新项目
-   "pc" 'project-compile ;; 执行编译命令
-   "pd" 'project-find-dir ;; 在 dired 中打开选定的文件夹
-   "pD" 'project-dired ;; 在项目根目录打开 dired
-   "pf" 'project-find-file ;; 在项目中搜索文件
-   "pF" 'project-or-external-find-file
-   "pp" 'project-switch-project ;; 切换项目
-   "pv" 'project-vc-dir
-   "px" 'project-execute-extended-command
-   "pr" 'project-query-replace-regexp
-   "pk" 'project-kill-buffers)
+    "p!" 'project-shell-command ;; 在项目根目录下执行命令
+    "p&" 'project-async-shell-command
+    "pa" 'project-remember-projects-under ;; 添加新项目
+    "pc" 'project-compile ;; 执行编译命令
+    "pd" 'project-find-dir ;; 在 dired 中打开选定的文件夹
+    "pD" 'project-dired ;; 在项目根目录打开 dired
+    "pf" 'project-find-file ;; 在项目中搜索文件
+    "pF" 'project-or-external-find-file
+    "pp" 'project-switch-project ;; 切换项目
+    "pv" 'project-vc-dir
+    "px" 'project-execute-extended-command
+    "pr" 'project-query-replace-regexp
+    "pk" 'project-kill-buffers)
   :config
   (cl-defmethod project-root ((project (head local)))
     "Return root directory of current PROJECT."
@@ -53,14 +55,16 @@
 
   (defun thomas/project-try-flag (dir)
     "Determine if DIR is a non-Git project."
-    (catch 'ret
-      (let ((pr-flags '((".project")
-                        ("go.mod" "Cargo.toml" "project.clj" "pom.xml" "package.json") ;; higher priority
-                        ("Makefile" "README.org" "README.md"))))
-        (dolist (current-level pr-flags)
-          (dolist (f current-level)
-            (when-let ((root (locate-dominating-file dir f)))
-              (throw 'ret (cons 'local root))))))))
+    (unless (member (expand-file-name "" dir)
+                    +project-ignore-project-list)
+      (catch 'ret
+        (let ((pr-flags '((".project")
+                          ("go.mod" "Cargo.toml" "project.clj" "pom.xml" "package.json") ;; higher priority
+                          ("Makefile" "README.org" "README.md"))))
+          (dolist (current-level pr-flags)
+            (dolist (f current-level)
+              (when-let ((root (locate-dominating-file dir f)))
+                (throw 'ret (cons 'local root)))))))))
 
   (setq project-find-functions '(project-try-vc thomas/project-try-flag)))
 
